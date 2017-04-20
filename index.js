3873fe408f9198f643e68097d6b6cc2e26cab4fb
Johnny Reina <johnny.r.reina@gmail.com>
2017-04-20 17:22:28 -0500

    su[p]

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 'use strict';
 
-const superlatives = [ 'beautiful', 'excellent', 'wonderful', 'impeccable', 'incredible' ];
-const randomInt = (min) => (max) => Math.floor(Math.random()*(max-min+1)+min); 
-console.log(superlatives[randomInt(0)(superlatives.length - 1)]);
+const up = 'most';
+const er = 0;
+const ve = [ 'beautiful', 'excellent', 'wonderful', 'impeccable', 'incredible' ];
+const la = ve.length;
+const ti = ' ';
+
+const s = v => w => x => y => z => v + y + z[Math.floor(Math.random()*(w-x+1)+x)];
+console.log(s(up)(er)(la)(ti)(ve));
